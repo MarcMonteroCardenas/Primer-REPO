99b0da9c54814f24ca294c0fcf2f5fbd8e8944f8
MarcMonteroCardenas <marc.montero.cardenas@gmail.com>
2026-09-25 10:25:13 +0200

    Create exercicis.py

---
+++ exercicis.py
@@ -0,0 +1,16 @@
+###
+# Exercicis - print()
+# Completa els exercicis següents utilitzant la funció print()
+###
+
+# Exercici 1
+# Imprimeix el teu nom i cognom a la consola
+print("marc montero cardenas")
+
+# Exercici 2
+# Imprimeix les paraules "Python", "és", "divertit" separades per un guió (-)
+# Pista: utilitza el paràmetre 'sep' de print()
+
+
+# Exercici 3
+# Imprimeix el resultat de la suma 15 + 27
